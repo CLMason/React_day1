bb792c5a41e78a40c1bde36c9301825d58ed4ba0
CLMason <clrumbold@gmail.com>
2019-10-03 12:01:45 -0400

    React day 1 notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; //{Component} = destructering. Getting things out from an object or library. 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//one way of making an app
+// function App() {
+//   return <h1>Hello World</h1>;
+// }
 
+class ExampleComponent extends Component {
+  render(){
+    return(
+    <fieldset>
+      <legend>I am an example</legend>
+      <p>Lorem ipsum</p>
+    </fieldset>
+    )
+  }
+}
+//using "extends" key word--inheriting from Component that already exists in React
+class App extends Component { //we can also say "React.Component" and it will work
+  render(){
+    let message = "hello world";
+    let items= ["bacon", "eggs","toast"];
+    return (
+      <div>
+        <h1>Hello World from a class!</h1>
+        <h2>This is an h2</h2>
+        <p>{message}</p>
+        <ul>
+          {
+            items.map( (item, index) =>
+            <li key = {index}>{item}</li>
+            )
+          }
+        </ul>
+        <ExampleComponent/>
+      </div>
+    )
+  }
+}
 export default App;
+
